f3812a15d3a609dc3833c9097fc99cf7a52122c0
Joao Gonçalves <xinelo_86@hotmail.com>
2026-01-25 23:21:48 +0000

    cors novo2

--- src/app.js
+++ src/app.js
@@ -1,34 +1,16 @@
 const express = require("express");
 const cors = require("cors");
-const path = require("path");
 
 const app = express();
-
-/* =========================
-   CORS — TEM DE VIR PRIMEIRO
-========================= */
 
 app.use(cors({
   origin: [
     "http://localhost:5173",
     "https://o-teu-frontend.onrender.com"
   ],
-  credentials: true,
 }));
 
-// 👉 responder explicitamente aos OPTIONS
-app.options("*", cors());
-
-/* =========================
-   MIDDLEWARES
-========================= */
-
 app.use(express.json());
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-
-/* =========================
-   ROTAS
-========================= */
 
 const userRoutes = require("./routes/user.routes");
 const flatRoutes = require("./routes/flat.routes");
@@ -37,10 +19,6 @@ const messageRoutes = require("./routes/message.routes");
 app.use("/users", userRoutes);
 app.use("/flats", flatRoutes);
 app.use("/flats", messageRoutes);
-
-/* =========================
-   TESTE
-========================= */
 
 app.get("/", (req, res) => {
   res.send("FlatFinder API is running 🚀");
