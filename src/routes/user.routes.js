e9e0d327272a9b7853c15a4c6e028af67aba5136
Joao Gonçalves <xinelo_86@hotmail.com>
2026-01-26 00:10:30 +0000

    fav ok3

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -13,18 +13,16 @@ router.post("/login", userController.login);
 // -------------------
 // Protected routes
 // -------------------
-router.get("/profile", auth, (req, res) => {
-  res.json({ message: "Access granted", userId: req.user.id });
-});
+router.get("/profile", auth, userController.getProfile); // opcional
+
+// Favorites routes (antes de /:id!)
+router.post("/favorites/:flatId", auth, userController.toggleFavorite);
+router.get("/favorites", auth, userController.getUserFavorites);
 
 // CRUD Users
 router.get("/", auth, adminMiddleware, userController.getAllUsers);
 router.get("/:id", auth, userController.getUserById);
 router.patch("/:id", auth, userController.updateUser);
 router.delete("/:id", auth, userController.deleteUser);
-
-// Favorites routes
-router.post("/favorites/:flatId", auth, userController.toggleFavorite);
-router.get("/favorites", auth, userController.getUserFavorites);
 
 module.exports = router;
