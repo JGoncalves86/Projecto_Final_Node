f2b26c0e6b4439e1dc7f6268cde75a2f55dc50bc
Joao Gonçalves <xinelo_86@hotmail.com>
2026-01-27 20:43:51 +0000

    mongodb ok

--- src/config/database.js
+++ src/config/database.js
@@ -1,16 +1,12 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-  const MONGO_URI = process.env.MONGO_URI;
   try {
-    await mongoose.connect(MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('MongoDB connected');
   } catch (err) {
     console.error('MongoDB connection error:', err);
-    process.exit(1); // encerra aplicação se falhar
+    process.exit(1);
   }
 };
 
