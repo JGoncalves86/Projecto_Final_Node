1a4167f57503672705c50e20f855672ebec3ef41
Joao Gonçalves <xinelo_86@hotmail.com>
2026-01-27 21:42:55 +0000

    user

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const userController = require('../controllers/userController');
+const userController = require('../controllers/user.controller');
 const authMiddleware = require('../middlewares/auth.middleware');
 const adminMiddleware = require('../middlewares/admin.middleware');
 
